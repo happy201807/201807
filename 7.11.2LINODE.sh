176080e04e2ed341f912ef3a013d93f2076ee7e7
happy201807 <143249384+happy201807@users.noreply.github.com>
2023-09-08 15:41:05 +0800

    Update 7.11.2LINODE.sh

--- 7.11.2LINODE.sh
+++ 7.11.2LINODE.sh
@@ -7,7 +7,7 @@ ADDRESS=`ip addr show eth0 | grep global | cut -d' ' -f 6 | head -n 1` && \
 GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
 echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
 /ip route add gateway=$GATEWAY
-/ip service disable telnet
+/ip service disable telnet ssh api api-ssl
 /user set 0 name=root password=$pass
  " > /mnt/rw/autorun.scr && \
 umount /mnt && \
