cf84411118430dbb3459c3797325f4a3a7768065
happy201807 <143249384+happy201807@users.noreply.github.com>
2023-09-10 18:06:42 +0800

    Update M.sh

--- Mikrotik_installer.sh
+++ Mikrotik_installer.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "Welcome to Cloudzy! MikroticOS will be installed on your VPS in a few seconds :)"
-wget https://download.mikrotik.com/routeros/6.44.6/chr-6.44.6.img.zip -O chr.img.zip  && \
+wget https://download.mikrotik.com/routeros/7.11.2/chr-7.11.2.img.zip -O chr.img.zip  && \
 gunzip -c chr.img.zip > chr.img  && \
-mount -o loop,offset=512 chr.img /mnt && \
-ADDRESS=`ip addr show eth0 | grep global | cut -d' ' -f 6 | head -n 1` && \
+mount -o loop,offset=33571840 chr.img /mnt && \
+ADDRESS=`ip addr show ens3 | grep global | cut -d' ' -f 6 | head -n 1` && \
 GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
 echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
 /ip route add gateway=$GATEWAY
-/ip service disable telnet
+/ip service disable telnet api api-ssl
 /user set 0 name=root password=$pass
  " > /mnt/rw/autorun.scr && \
 umount /mnt && \
